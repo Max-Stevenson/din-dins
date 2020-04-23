67748452ede82122a8c5d485190e73035e129802
Max Stevenson <maxstevenson@msn.com>
2020-04-23 11:40:51 +0100

    Added database config to backened.
    - removed unused scripts from config.
    - updated gitignore.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@dindins-cluster-rxgr4.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is up on port:${port}`);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
