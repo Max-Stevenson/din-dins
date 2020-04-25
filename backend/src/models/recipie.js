9e05c39bbc6d2f8b9a258aefd02d66b01dfdb806
Max Stevenson <maxstevenson@msn.com>
2020-04-25 23:21:54 +0100

    Upadated recipie schema.
    - Started on recipie creation route.

--- backend/src/models/recipie.js
+++ backend/src/models/recipie.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipieSchema = new Schema({
-  // image: { data: Buffer, content: String},
+  image: { data: Buffer, content: String },
   title: { type: String, required: true },
-  
-
+  isVegetarian: { type: boolean, required: true },
+  mealSize: { type: number, default: 1 },
+  ingredients: { type: [String], required: true },
+  method: { type: [String], required: true }
 });
 
 module.exports = mongoose.model("Recipie", recipieSchema);
--- backend/src/routes/recipie-routes.js
+++ backend/src/routes/recipie-routes.js
@@ -1,2 +1,6 @@
 const express = require('express');
 const { check } = require('express-validator');
+const fileUpload = require('../middleware/file-upload');
+const router = express.Router();
+
+router.post("/recipies", fileUpload.single('image'), [ check("title").not().isEmpty(), check("desc")])
