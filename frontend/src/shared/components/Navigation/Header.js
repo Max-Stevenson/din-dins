232a0e822e6a6b1adbb28aca477981f82a20c0ea
Max Stevenson <maxstevenson@msn.com>
2020-05-05 23:25:26 +0100

    WIP on mobile SideDrawer component.

--- frontend/src/shared/components/Navigation/Header.js
+++ frontend/src/shared/components/Navigation/Header.js
@@ -2,22 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
 const Header = props => {
   return (
-    <header className="main-header">
-      <div className="main-header__menu-btn">
-        <div />
-        <div />
-        <div />
-      </div>
-      <h1 className="main-header__title">
-        <Link to="/" exact>Din Dins</Link>
-      </h1>
-      <nav className="main_header__nav-links">
-        <NavLinks />
-      </nav>
-    </header>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-header__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <header className="main-header">
+        <div className="main-header__menu-btn">
+          <div />
+          <div />
+          <div />
+        </div>
+        <h1 className="main-header__title">
+          <Link to="/" exact>
+            Din Dins
+          </Link>
+        </h1>
+        <nav className="main_header__nav-links">
+          <NavLinks />
+        </nav>
+      </header>
+    </React.Fragment>
   );
 };
 
