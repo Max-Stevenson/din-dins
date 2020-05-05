f13797965d1ad02331662c99a7ae572690c0c375
Max Stevenson <maxstevenson@msn.com>
2020-05-05 23:42:13 +0100

    Continued work on SideDrawer.
    - Implemented state in Header to track drawer status.

--- frontend/src/shared/components/Navigation/Header.js
+++ frontend/src/shared/components/Navigation/Header.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
 
 const Header = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
+      {drawerIsOpen && (<SideDrawer>
         <nav className="main-header__drawer-nav">
           <NavLinks />
         </nav>
-      </SideDrawer>
+      </SideDrawer>)}
       <header className="main-header">
-        <div className="main-header__menu-btn">
+        <button className="main-header__menu-btn" onClick={openDrawer}>
           <div />
           <div />
           <div />
-        </div>
+        </button>
         <h1 className="main-header__title">
           <Link to="/" exact>
             Din Dins
