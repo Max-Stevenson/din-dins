521a4cf9e45f7325faa876bc09a35859dba48e83
Max Stevenson <maxstevenson@msn.com>
2020-05-08 12:53:00 +0100

    Implemented state in Input component.
    - Created validators for use in custom Input comp.

--- frontend/src/recipes/pages/NewRecipe.js
+++ frontend/src/recipes/pages/NewRecipe.js
@@ -7,7 +7,13 @@ const NewRecipe = () => {
     <React.Fragment>
       <h2>New Recipe Page</h2>
       <form className="recipe-form">
-        <Input element="input" type="text" label="Title"/>
+        <Input
+          element="input"
+          type="text"
+          label="Title"
+          validators={[]}
+          errorText="Please enter valid recipe title."
+        />
       </form>
     </React.Fragment>
   );
--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -1,31 +1,66 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { validate } from "../../util/validators";
 import "./Input.css";
 
-const Input = (props) => {
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: true
+      };
+    case "TOUCH":
+      return {
+        ...state,
+        isTouched: true
+      };
+    default:
+      return state;
+  }
+};
+
+const Input = props => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false
+  });
+  const changeHandler = event => {
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value
+    });
+  };
+
   const element =
     props.element === "input" ? (
       <input
         id={props.id}
         type={props.type}
         placeholder={props.placeholder}
-        // onChange={changeHandler}
+        onChange={changeHandler}
         // onBlur={touchHandler}
-        // value={inputState.value}
+        value={inputState.value}
       />
     ) : (
       <textarea
         id={props.id}
         rows={props.rows || 3}
-        // onChange={changeHandler}
+        onChange={changeHandler}
         // onBlur={touchHandler}
-        // value={inputState.value}
+        value={inputState.value}
       />
     );
 
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${!inputState.isValid &&
+        inputState.isTouched &&
+        "form-control--invalid"}`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
   );
 };
