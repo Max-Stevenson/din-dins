20190ad05124eac21c00fe69ceca7c778e6401b5
Max Stevenson <maxstevenson@msn.com>
2020-05-08 12:15:32 +0100

    Created custom Input comp.

--- frontend/src/recipes/pages/NewRecipe.js
+++ frontend/src/recipes/pages/NewRecipe.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+import "./NewRecipe.css";
 
 const NewRecipe = () => {
-  return (<h2>New Recipe Page</h2>)
+  return (
+    <React.Fragment>
+      <h2>New Recipe Page</h2>
+      <form className="recipe-form">
+        <Input element="input" type="text" label="Title"/>
+      </form>
+    </React.Fragment>
+  );
 };
 
 export default NewRecipe;
