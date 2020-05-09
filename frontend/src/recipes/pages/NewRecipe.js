1f6d36498af41efadc3a684dd3b88417e376b26d
Max Stevenson <maxstevenson@msn.com>
2020-05-09 11:34:58 +0100

    WIP on new recipe page.
    - Created planned comps for adding ingrediants and cooking steps.

--- frontend/src/recipes/pages/NewRecipe.js
+++ frontend/src/recipes/pages/NewRecipe.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Input from "../../shared/components/FormElements/Input";
 import "./NewRecipe.css";
 import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 const NewRecipe = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <React.Fragment>
       <h2>New Recipe Page</h2>
@@ -14,7 +16,16 @@ const NewRecipe = () => {
           label="Title"
           validators={[VALIDATOR_REQUIRE()]}
           errorText="Please enter valid recipe title."
+          onInput={titleInputHandler}
         />
+        <label htmlFor="quantity">Number of meals:</label>
+        <input type="number" id="quantity" name="quantity" min="1" />
+
+        <label for="vegetarian">Vegetarian</label>
+        <input type="radio" id="vegetarian" name="meatType" value="vegetarian" />
+        <label for="non-vegetarian">Non-vegetarian</label>
+        <input type="radio" id="non-vegetarian" name="meatType" value="non-vegetarian" />
+        
       </form>
     </React.Fragment>
   );
