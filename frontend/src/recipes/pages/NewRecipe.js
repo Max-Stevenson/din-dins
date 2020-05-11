7c1cf543900e4c05a79ca190d737835f207f965f
Max Stevenson <maxstevenson@msn.com>
2020-05-11 15:30:51 +0100

    Messy merge of ingredient functionality into NewRecipe.
    - Needs refactor but can now add and remove individual ingredient items.

--- frontend/src/recipes/pages/NewRecipe.js
+++ frontend/src/recipes/pages/NewRecipe.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import "./NewRecipe.css";
 import { VALIDATOR_REQUIRE, VALIDATOR_MIN } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return {
+        ingredients: [...state.ingredients, { ingredient: action.ingredient }]
+      };
+    case "REMOVE":
+      return {
+        ingredients: state.ingredients.filter(si => {
+          return si.ingredient.item !== action.i.ingredient.item;
+        })
+      };
+    default:
+      return state;
+  }
+};
+
 const NewRecipe = () => {
+  const [{ ingredients }, dispatch] = useReducer(reducer, { ingredients: [] });
+  const [quantity, setQuantity] = useState(0);
+  const [measure, setMeasure] = useState("");
+  const [item, setItem] = useState("");
   const [formState, inputHandler] = useForm(
     {
       title: {
@@ -19,23 +40,25 @@ const NewRecipe = () => {
       mealType: {
         value: "",
         isValid: false
-      },
-      ingredients: {
-        value: ["chicken", "chips"],
-        isValid: false
       }
     },
     false
   );
 
-  const handleIngredientAdd = event => {
-    event.preventDefault();
-    
+  const handleAdd = e => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_ITEM",
+      ingredient: { quantity, measure, item }
+    });
+    setQuantity(0);
+    setMeasure("");
+    setItem("");
   };
 
   const recipeSubmitHandler = event => {
     event.preventDefault();
-    console.log(formState.inputs);
+    console.log({...formState.inputs, ingredients});
   };
 
   return (
@@ -86,43 +109,45 @@ const NewRecipe = () => {
         <div className="recipe-form__ingredients-list">
           <h2>Ingredients</h2>
           <ul>
-            {formState.inputs.ingredients.value.map(ing => (
-              <li>{ing}</li>
+            {ingredients.map((i, idx) => (
+              <li key={idx}>
+                {i.ingredient.quantity} {i.ingredient.measure}{" "}
+                {i.ingredient.item}
+                <button
+                  onClick={e => {
+                    e.preventDefault();
+                    dispatch({ type: "REMOVE", i });
+                  }}
+                >
+                  Remove
+                </button>
+              </li>
             ))}
           </ul>
-          <Input
-            element="input"
-            id="quantity"
-            name="quantity"
+          <label htmlFor="ingredientQuantity">Quantity</label>
+          <input
             type="number"
+            id="ingredientQuantity"
             label="Quantity"
-            validators={[VALIDATOR_REQUIRE()]}
-            onInput={inputHandler}
-            value={0}
-            errorText="Please enter quantity of ingredient."
+            onChange={e => setQuantity(e.target.value)}
+            value={quantity}
           />
-          <Input
-            element="input"
-            id="measurement"
-            name="measurement"
+          <label htmlFor="ingredientMeasure">Measure</label>
+          <input
             type="text"
+            id="ingredientMeasure"
             label="Measurement"
-            validators={[]}
-            onInput={inputHandler}
-            value={""}
+            onChange={e => setMeasure(e.target.value)}
+            value={measure}
           />
-          <Input
-            element="input"
-            id="ingredient"
-            name="ingredient"
+          <label htmlFor="ingredient">ingredient</label>
+          <input
             type="text"
-            label="ingredient"
-            validators={[VALIDATOR_REQUIRE()]}
-            onInput={inputHandler}
-            value={""}
-            errorText="Please enter an ingredient."
+            id="ingredient"
+            onChange={e => setItem(e.target.value)}
+            value={item}
           />
-          <Button onClick={handleIngredientAdd}>Add Ingredient</Button>
+          <Button onClick={handleAdd}>Add Ingredient</Button>
         </div>
         <Button type="submit" disabled={!formState.isValid}>
           Add Recipe
