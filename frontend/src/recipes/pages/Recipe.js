57210c6dd63907c4df824c186d2ee296b6cfaffd
Max Stevenson <maxstevenson@msn.com>
2020-05-12 10:17:53 +0100

    Updated Recipe page.
    - Changed dummy data format to better reflect form results.

--- frontend/src/recipes/pages/Recipe.js
+++ frontend/src/recipes/pages/Recipe.js
@@ -3,16 +3,20 @@ import { useParams } from "react-router-dom";
 import Tabs from "../../shared/components/UIElements/Tabs";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import { VALIDATOR_REQUIRE, VALIDATOR_MIN } from "../../shared/util/validators";
 import "./Recipe.css";
+import { useForm } from "../../shared/hooks/form-hook";
 
 const testItems = [
   {
     mealSize: 2,
     image:
       "https://images-gmi-pmc.edge-generalmills.com/7d6f3a8e-2eca-4c61-8988-489b40546395.jpg",
-    ingredients: ["Chicken", "Vegetables"],
-    method: ["Season chicken.", "Cook at 220c for 1.5 hours."],
+    ingredients: [
+      { ingredient: { quantity: 1, measure: "cup", item: "flour" } },
+      { ingredient: { quantity: 2, measure: "", item: "carrots" } }
+    ],
+    method: [{ step: "prehead oven" }, { step: "cook" }],
     id: "5eaecf2a95ab162c225464c5",
     title: "Chicken Dinner",
     isVegetarian: false,
@@ -22,8 +26,11 @@ const testItems = [
     mealSize: 3,
     image:
       "https://realfood.tesco.com/media/images/Burger-31LGH-a296a356-020c-4969-86e8-d8c26139f83f-0-1400x919.jpg",
-    ingredients: ["beef", "buns"],
-    method: ["Make Burger.", "Cook at 220c for 20 mins."],
+    ingredients: [
+      { ingredient: { quantity: 1, measure: "cup", item: "flour" } },
+      { ingredient: { quantity: 2, measure: "", item: "carrots" } }
+    ],
+    method: [{ step: "prehead oven" }, { step: "cook" }],
     id: "7eaecf2a95ab162c225464a6",
     title: "Burgers",
     isVegetarian: false,
@@ -35,6 +42,11 @@ const Recipe = () => {
   const recipeId = useParams().recipeId;
 
   const identifiedRecipe = testItems.find(p => p.id === recipeId);
+
+  const [formState, inputHandler] = useForm(
+    { title: { value: identifiedRecipe.title, isValid: true } },
+    true
+  );
 
   if (!identifiedRecipe) {
     return (
@@ -54,27 +66,42 @@ const Recipe = () => {
           label="Title"
           validators={[VALIDATOR_REQUIRE()]}
           errorText="Please enter valid recipe title."
-          onInput={() => {}}
+          onInput={inputHandler}
           value={identifiedRecipe.title}
+          valid={true}
+        />
+        <Input
+          element="input"
+          id="mealSize"
+          type="number"
+          label="meal size"
+          validators={[VALIDATOR_MIN(1)]}
+          errorText="Please enter a valid meal size."
+          onInput={inputHandler}
+          value={identifiedRecipe.mealSize}
           valid={true}
         />
         <Tabs>
           <div label="Ingredients">
             <ul>
-              {identifiedRecipe.ingredients.map(ingredient => (
-                <li key={ingredient}>{ingredient}</li>
+              {identifiedRecipe.ingredients.map((ingredient, idx) => (
+                <li key={idx}>
+                  {ingredient.ingredient.quantity} {ingredient.ingredient.measure} {ingredient.ingredient.item}
+                </li>
               ))}
             </ul>
           </div>
           <div label="Method">
             <ul>
-              {identifiedRecipe.method.map(step => (
-                <li key={step}>{step}</li>
+              {identifiedRecipe.method.map((step, idx) => (
+                <li key={idx}>{step.step}</li>
               ))}
             </ul>
           </div>
         </Tabs>
-        <Button type="submit" disabled={true}>Update Recipe</Button>
+        <Button type="submit" disabled={true}>
+          Update Recipe
+        </Button>
       </form>
     </div>
   );
--- frontend/src/recipes/pages/Recipes.js
+++ frontend/src/recipes/pages/Recipes.js
@@ -1,25 +1,36 @@
 import React from "react";
 import RecipesList from "../components/RecipesList";
 
-const testItems = [{
-  mealSize: 2,
-  image: "https://images-gmi-pmc.edge-generalmills.com/7d6f3a8e-2eca-4c61-8988-489b40546395.jpg",
-  ingredients: ["Chicken", "Vegetables"],
-  method: ["Season chicken.", "Cook at 220c for 1.5 hours."],
-  id: "5eaecf2a95ab162c225464c5",
-  title: "Chicken Dinner",
-  isVegetarian: false,
-  __v: 0
-}, {
-  mealSize: 3,
-  image: "https://realfood.tesco.com/media/images/Burger-31LGH-a296a356-020c-4969-86e8-d8c26139f83f-0-1400x919.jpg",
-  ingredients: ["beef", "buns"],
-  method: ["Make Burger.", "Cook at 220c for 20 mins."],
-  id: "7eaecf2a95ab162c225464a6",
-  title: "Burgers",
-  isVegetarian: false,
-  __v: 0
-}];
+const testItems = [
+  {
+    mealSize: 2,
+    image:
+      "https://images-gmi-pmc.edge-generalmills.com/7d6f3a8e-2eca-4c61-8988-489b40546395.jpg",
+    ingredients: [
+      { ingredient: { quantity: 1, measure: "cup", item: "flour" } },
+      { ingredient: { quantity: 2, measure: "", item: "carrots" } }
+    ],
+    method: [{ step: "prehead oven" }, { step: "cook" }],
+    id: "5eaecf2a95ab162c225464c5",
+    title: "Chicken Dinner",
+    isVegetarian: false,
+    __v: 0
+  },
+  {
+    mealSize: 3,
+    image:
+      "https://realfood.tesco.com/media/images/Burger-31LGH-a296a356-020c-4969-86e8-d8c26139f83f-0-1400x919.jpg",
+    ingredients: [
+      { ingredient: { quantity: 1, measure: "cup", item: "flour" } },
+      { ingredient: { quantity: 2, measure: "", item: "carrots" } }
+    ],
+    method: [{ step: "prehead oven" }, { step: "cook" }],
+    id: "7eaecf2a95ab162c225464a6",
+    title: "Burgers",
+    isVegetarian: false,
+    __v: 0
+  }
+];
 
 const Recipes = () => {
   return (
