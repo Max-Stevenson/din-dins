dba306bc56c36936c2391ef268deecac0fa30b3e
Max Stevenson <maxstevenson@msn.com>
2020-05-13 18:43:11 +0100

    Finished Login page.

--- frontend/src/profile/pages/Auth.js
+++ frontend/src/profile/pages/Auth.js
@@ -1,8 +1,56 @@
 import React from "react";
 import "./Auth.css";
+import Card from "../../shared/components/UIElements/Card";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_REQUIRE
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
 
 const Auth = () => {
-  return <h2>Auth Page</h2>;
+  const [formState, inputHandler] = useForm(
+    {
+      email: { value: "", isValid: false },
+      password: { value: "", isValid: false }
+    },
+    false
+  );
+
+  const authSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <Card class="authentication">
+      <h2>Login Required</h2>
+      <form>
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="Email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter valid email address."
+          onInput={inputHandler}
+        />
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter your password."
+          onInput={inputHandler}
+        />
+        <Button type="submit" onClick={authSubmitHandler} disabled={!formState.isValid}>
+          Login
+        </Button>
+      </form>
+    </Card>
+  );
 };
 
 export default Auth;
