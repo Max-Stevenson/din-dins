8108790534a7678b2955a3db4695ae2d6291ee43
Max Stevenson <maxstevenson@msn.com>
2020-05-14 16:00:23 +0100

    Updated model on backend to fit new data model sent by frontend.

--- backend/src/models/recipe.js
+++ backend/src/models/recipe.js
@@ -5,9 +5,18 @@ const Schema = mongoose.Schema;
 const recipeSchema = new Schema({
   image: { data: Buffer, content: String },
   title: { type: String, trim: true, required: true },
-  isVegetarian: { type: Boolean, required: true },
+  isVegetarian: { type: String, required: true },
   mealSize: { type: Number, default: 1 },
-  ingredients: { type: [String], required: true },
+  ingredients: {
+    type: [
+      {
+        quantity: { type: Number, required: true },
+        measure: { type: String },
+        item: { type: String, required: true }
+      }
+    ],
+    required: true
+  },
   method: { type: [String], required: true }
 });
 
