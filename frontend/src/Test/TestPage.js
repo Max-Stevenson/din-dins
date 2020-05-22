efdc9d236da42124027150b7466de534e2a9a6ed
Max Stevenson <maxstevenson@msn.com>
2020-05-22 10:49:24 +0100

    WIP on mealplanner.
    - Can now intereact with Day component exposing recipe selection modal.

--- frontend/src/Test/TestPage.js
+++ frontend/src/Test/TestPage.js
@@ -1,11 +1,36 @@
-import React, { useState, useReducer } from "react";
-import { format, formatDistance, formatRelative, subDays } from "date-fns";
+import React, { useState, useReducer, useEffect } from "react";
+import { format, formatDistance, formatRelative, addDays } from "date-fns";
 import Button from "../shared/components/FormElements/Button";
 import "./TestPage.css";
 import Modal from "../shared/components/UIElements/Modal";
 import RecipesList from "../recipes/components/RecipesList";
 
 const TEST_DATA = [
+  {
+    mealSize: 2,
+    _id: "5ec13c8319a4052774dd30b7",
+    title: "Chicken Winner",
+    isVegetarian: "false",
+    image:
+      "https://images-gmi-pmc.edge-generalmills.com/7d6f3a8e-2eca-4c61-8988-489b40546395.jpg",
+    ingredients: [
+      {
+        ingredient: {
+          quantity: 1,
+          measure: "",
+          item: "whole chicken"
+        },
+        _id: "5ec13c8319a4052774dd30b8"
+      }
+    ],
+    method: [
+      {
+        _id: "5ec13c8319a4052774dd30b9",
+        step: "preheat oven"
+      }
+    ],
+    __v: 0
+  },
   {
     mealSize: 2,
     _id: "5ec13c8319a4052774dd30b7",
@@ -39,10 +64,13 @@ const Day = props => {
 
   const showRecipeModalHandler = () => {
     setShowRecipeModal(true);
+    document.body.style.overflow = "hidden";
   };
 
   const cancelRecipeModalHandler = () => {
     setShowRecipeModal(false);
+    document.body.style.overflow = 'unset';
+
   };
 
   return (
@@ -63,20 +91,27 @@ const Day = props => {
       </Modal>
       <div className="day-container" onClick={showRecipeModalHandler}>
         {props.date}
-        {plannedRecipe && plannedRecipe}
+        <div className="day-container__recipe">
+          {plannedRecipe && plannedRecipe}
+        </div>
       </div>
     </React.Fragment>
   );
 };
 
 const TestPage = () => {
-  const [currentDate, setCurrentDate] = useState(format(new Date(), "dd-MMM"));
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const nextDay = addDays(currentDate, 1);
 
+  useEffect(() => {
+    console.log(nextDay);
+  }, []);
   return (
     <div>
       <h2>TestPage</h2>
       <div className="root-element">
-        <Day date={currentDate} />
+        <Day date={format(currentDate, "dd-MMM")} />
+        <Day date={format(nextDay, "dd-MMM")} />
       </div>
     </div>
   );
